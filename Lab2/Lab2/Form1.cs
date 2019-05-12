f4a264b610eb2bc885be5a1719ec66cb810e2acc
vi24 <31028156+vi24@users.noreply.github.com>
2019-05-13 01:09:58 +0200

    Lab 2: Added controls

---
+++ Lab2/Lab2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class TheQuestForm : Form
+    {
+        public TheQuestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
